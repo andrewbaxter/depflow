5566ba0f91a64bbe241a35b4554302ba429605d8
Andrew Baxter <afb2@wustl.edu>
2016-11-07 16:32:58 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='depflow',
+    version='0.0.1',
+    author='Andrew Baxter',
+    author_email='afb2@wustl.edu',
+    url='https://github.com/andrewbaxter/depflow',
+    license='BSD',
+    py_modules=['depflow'],
+)
